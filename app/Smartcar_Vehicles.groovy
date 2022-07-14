7bce49ee921f2c2f45f34693d56b3192c928896f
fieldsjm <39744574+fieldsjm@users.noreply.github.com>
2022-07-14 10:19:07 -0400

    Update Smartcar_Vehicles.groovy

--- app/Smartcar_Vehicles.groovy
+++ app/Smartcar_Vehicles.groovy
@@ -10,6 +10,7 @@
 definition(
 	name: "Smartcar Vehicles",
 	author: "Jonathan Fields",
+	namespace: "jmf.smartcar",
 	description: "Integrate Smartcar into Hubitat.",
 	category: "My Apps",
 	parent: "Smartcar Integration"
@@ -17,10 +18,8 @@ definition(
 	iconX2Url: "https://raw.githubusercontent.com/fieldsjm/Hubitat-Smartcar/main/resources/smartcar_logo@2x.png",
 	iconX3Url: "https://raw.githubusercontent.com/fieldsjm/Hubitat-Smartcar/main/resources/smartcar_logo@3x.png",
 	documentationLink: "https://github.com/fieldsjm/Hubitat-Smartcar/blob/main/README.md")
-	
 
 preferences {
-    page(name: "prefMain")
 	page(name: "prefOAuth")
 	page(name: "prefDevices")
 }
@@ -29,39 +28,28 @@ preferences {
 	imperial: 1,
 	metric: 2
 ]
-
-def prefMain() {
-	return dynamicPage(name: "prefMain", title: "Smartcar Integration", nextPage: "prefOAuth", uninstall:false, install: false) {
-		section {
-			input "vehicleName", "text", title: "The Vehicle Bame associated with this app", required: true
-		}
-	}
-}
 def prefOAuth() {
-	return dynamicPage(name: "prefOAuth", title: "Withings OAuth", nextPage: "prefDevices", uninstall:false, install: false) {
-		section {	
-			def desc = ""
-			if (!state.authToken) {
-				showHideNextButton(false)
-				desc = "To continue you will need to connect your Smartcar and Hubitat accounts"
-			}
-			else {
-				showHideNextButton(true)
-				desc = "Your Hubitat and Smartcar accounts are connected"
-			}
-			href url: oauthInitialize(), style: "external", required: true, title: "Smartcar Account Authorization", description: desc
-		}
-	}
+    return dynamicPage(name: "prefOAuth", title: "Smartcar OAuth", nextPage: "prefDevices", uninstall:false, install: false) {
+        section {
+            def desc = ""
+            if (!state.authToken) {
+                showHideNextButton(false)
+                desc = "To continue you will need to connect your Smartcar and Hubitat accounts"
+            } else {
+                showHideNextButton(true)
+                desc = "Your Hubitat and Smartcar accounts are connected"
+            }
+            href url: oauthInitialize(), style: "external", required: true, title: "Smartcar Account Authorization", description: desc
+        }
+    }
 }
 
 def prefDevices() {
-	app.updateLabel("${vehicleName}")
-	state.devices = getSmartcarDevices() 
+	state.devices = getSmartcars() 
 	return dynamicPage(name: "prefDevices", title: "Smartcar Vehicles", uninstall:true, install: true) {
-		section {
-			if (state.devices?.vehicles?.size() > 0)
-				input "vehicles", "enum", title: "Vehicles", options: state.devices.vehicles, multiple: true}
-		}
+        section {
+            if (state.devices?.vehicles?.size() > 0) input "vehicles", "enum", title: "Vehicles", options: state.devices.vehicles, multiple: true
+        }
 	}
 }
 
@@ -81,6 +69,12 @@ mappings {
 			GET: "oauthCallback"
 		]
 	}
+	path("/notification/:type") {
+		action: [
+			GET: "withingsNotification",
+			POST: "withingsNotification"
+		]
+	}
 }
 
 // OAuth Routines
@@ -91,24 +85,23 @@ def oauthInitialize() {
 		createAccessToken()
 
 	state.oauthState = "${getHubUID()}/apps/${app.id}/callback?access_token=${state.accessToken}"
-	
-	/*Compare to Postman*/
+		
 	return "https://account.withings.com/oauth2_user/authorize2?response_type=code&client_id=${oauthInfo.clientID}&scope=${URLEncoder.encode("user.info,user.activity,user.sleepevents,user.metrics")}&redirect_uri=${URLEncoder.encode("https://cloud.hubitat.com/oauth/stateredirect")}&state=${URLEncoder.encode(state.oauthState)}"
 }
 
 def oauthCallback() {
 	if (params.state == state.oauthState) {
 		def oauthInfo = parent.getOAuthDetails()
-        try { 
+try { 
             httpPost([
-				uri: "https://wbsapi.withings.net",
-				path: "/v2/oauth2",
+				uri: "https://auth.smartcar.com",
+				path: "/oauth/token",
+		    
 				body: [
-					"action": "requesttoken",
 					"grant_type": "authorization_code",
 					code: params.code,
-					client_id : oauthInfo.clientID,
-					client_secret: oauthInfo.clientSecret,
+					client_id : clientID,
+					client_secret: clientSecret,
 					redirect_uri: "https://cloud.hubitat.com/oauth/stateredirect"
 				]
 			]) { resp ->
@@ -123,14 +116,14 @@ def oauthCallback() {
 		catch (e) {
             log.error "OAuth error: ${e}"
         }
-	} 
-	else {
-		log.error "OAuth state does not match, possible spoofing?"
-	}
-	if (state.authToken) 
-		oauthSuccess()
-	else
-		oauthFailure()
+	} else {
+        log.error "OAuth state does not match, possible spoofing?"
+    }
+    if (state.authToken) {
+        oauthSuccess()
+    } else {
+        oauthFailure()
+    }
 }
 
 def oauthSuccess() {
@@ -149,16 +142,14 @@ def oauthFailure() {
 
 def refreshToken() {
 	def result = false
-	try {
-		def oauthInfo = parent.getOAuthDetails()
+try {
 		def params = [
-			uri: "https://wbsapi.withings.net",
-			path: "/v2/oauth2",
+			uri: "https://auth.smartcar.com/",
+			path: "/oauth/token",
 			body: [
-				"action": "requesttoken",
 				grant_type: "refresh_token",
-				client_id: oauthInfo.clientID,
-				client_secret: oauthInfo.clientSecret,
+				client_id: clientID,
+				client_secret: clientSecret,
 				refresh_token: state.refreshToken
 			]
 		]
@@ -168,106 +159,53 @@ def refreshToken() {
                 state.authToken = resp.data.body.access_token
                 state.authTokenExpires = now() + (resp.data.body.expires_in * 1000) - 60000
 				result = true
-			}
-			else {
-				state.authToken = null
-				result = false
+			} else {
+                state.authToken = null
+                result = false
 			}
 		}
-	}
-	catch (e) {
+	} catch (e) {
 		log.error "Failed to refresh token: ${e}"
 		state.authToken = null
 		result = false
-	}
-	return result
+    }
+    return result
 }
 
-def getSmartcarDevices() {
-	def vehicles = [:]
-	def body = apiGet("", null)
-	for (device in body.vehicles) {
-		vehicle[device.deviceid] = device.id
-	}
-	return [vehicle: vehicles]
+def getSmartcars() {
+	def vehicles = apiGetVehicles()
+	log.info vehicles
 }
 
-def callbackUrl(type) {
-	// This looks insecure but it's really not. The Withings API apparently requires HTTP (what???)
-	// But, on the HE side HSTS is supported so it redirects right to HTTPS.
-	return "${getFullApiServerUrl()}/notification/${type}?access_token=${state.accessToken}".replace("https://", "http://")
-}
-
-def getLocation(id) {
-	def data = apiGet("location", id)
-
-	for (item in data) {
-		def dev = null
-
-		if (item.latitidue != null) dev = getChildDevice(buildDNI(item.deviceid))
-
-		if (!dev)
-			continue
-
-		dev.sendEvent(name: "Latitude", value: item.latitude, isStateChange: true)
-		dev.sendEvent(name: "Longitude", value: item.longitude, isStateChange: true)
-	}
-}
-
-def sendEventsForMeasurements(dev, measurements, types) {
-	for (measurement in measurements) {
-		if (types.contains(measurement.type)) {
-			def attrib = measures[measurement.type].attribute
-			def displayAttrib = measures[measurement.type].displayAttribute
-			def result = measures[measurement.type].converter.call(measurement.value, measurement.unit)
-			dev.sendEvent(name: attrib, value: result.value, unit: result.unit, isStateChange: true)
-			if (displayAttrib != null)
-				dev.sendEvent(name: displayAttrib, value: result.displayValue, isStateChange: true)
-		}
-	}
-}
-
-def getChildByCapability(capability) {
-	def childDevices = getChildDevices()
-	for (dev in childDevices) {
-		if (dev.hasCapability(capability))
-			return dev
-	}
-	return null
-}
-
-// API call methods
-
-def apiGet(endpoint, id) {
-	logDebug "${endpoint}---${id}"
-	def path = null
-	if (id == null) {
-		path = ""
-	} else {
-		path = "/" + id + "/" + endpoint
-	}
-		
-	if (state.authTokenExpires <= now()) {
-		if (!refreshToken())
-			return null
-	}
-	def result = null
-	try {
-		def params = [
-			uri: "https://api.smartcar.com/v2.0/vechicles",
-			path: path,
+def apiGetVehicles() {
+    if (state.authTokenExpires <= now()) {
+        if (!refreshToken())
+        return null
+    }
+    def result = null
+    try {
+        def params = [
+			uri: "https://api.smartcar.com",
+			path: "/v2.0/vehicles",
 			contentType: "application/json",
 			headers: [
 				"Authorization": "Bearer " + state.authToken
-				"SC-Unit-System": + parent.getMeasurementSystem()
 			]
 		]
-	}
-	catch (e) {
-		log.error "Error getting API data ${endpoint}---${id}: ${e}"
-		result = null
-	}
-	return result
+		if (query != null)
+			params.query << query
+		httpGet(params) { resp ->
+            if (resp.data.status == 0) {
+                result = resp.data.body
+            } else if (resp.data.status == 401) {
+                refreshToken()
+            }
+		}
+	} catch (e) {
+        log.error "Error getting API data for vehicles: ${e}"
+        result = null
+    }
+    return result
 }
 
 def installed() {
@@ -276,37 +214,62 @@ def installed() {
 
 def uninstalled() {
 	logDebug "uninstalling app"
+	unsubscribeWithingsNotifications()
 	removeChildDevices(getChildDevices())
 }
 
 def updated() {	
     logDebug "Updated with settings: ${settings}"
 	unschedule()
+    unsubscribe()
 	initialize()
 }
 
 def initialize() {
 	cleanupChildDevices()
 	createChildDevices()
+	updateSubscriptions()
 	schedule("0 */30 * * * ? *", refreshDevices)
 }
 
 def buildDNI(deviceid) {
-	return "smartcar:${state.userid}:${deviceid}"
+	return "withings:${state.userid}:${deviceid}"
 }
 
 def createChildDevices() {
-	for (vehicle in vehicles) {
-            addChildDevice( "Smartcar Vehicle", buildDNI(scale), 1234, ["name": "${vehicle} ${state.devices.vehicle[vehicle]}", isComponent: false])
-	}	
+	for (scale in scales)
+	{
+		if (!getChildDevice(buildDNI(scale)))
+            addChildDevice("dcm.withings", "Withings Scale", buildDNI(scale), 1234, ["name": "${userName} ${state.devices.scales[scale]}", isComponent: false])
+	}
+	for (sleepMonitor in sleepMonitors)
+	{
+		if (!getChildDevice(buildDNI(sleepMonitor)))
+            addChildDevice("dcm.withings", "Withings Sleep Sensor", buildDNI(sleepMonitor), 1234, ["name": "${userName} ${state.devices.sleepMonitors[sleepMonitor]}", isComponent: false])
+	}
+	for (activityTracker in activityTrackers)
+	{
+		if (!getChildDevice(buildDNI(activityTracker)))
+            addChildDevice("dcm.withings", "Withings Activity Tracker", buildDNI(activityTracker), 1234, ["name": "${userName} ${state.devices.activityTrackers[activityTracker]}", isComponent: false])
+	}
+	for (bp in bloodPressure)
+	{
+		if (!getChildDevice(buildDNI(bp)))
+            addChildDevice("dcm.withings", "Withings Blood Pressure Monitor", buildDNI(bp), 1234, ["name": "${userName} ${state.devices.bloodPressure[bp]}", isComponent: false])
+	}
+	for (thermometer in thermometers)
+	{
+		if (!getChildDevice(buildDNI(thermometer)))
+            addChildDevice("dcm.withings", "Withings Thermometer", buildDNI(thermometer), 1234, ["name": "${userName} ${state.devices.thermometers[thermometer]}", isComponent: false])
+	}
 }
 
 def cleanupChildDevices()
 {
 	for (device in getChildDevices())
 	{
-		def deviceId = device.deviceNetworkId.replace("smartcar:","")
-		def allDevices = (vehicles ?: [])
+		def deviceId = device.deviceNetworkId.replace("withings:","")
+		def allDevices = (scales ?: []) + (sleepMonitors ?: []) + (activityTrackers ?: []) + (bloodPressure ?: []) + (thermometers ?: [])
 		def deviceFound = false
 		for (dev in allDevices)
 		{
@@ -331,9 +294,21 @@ private removeChildDevices(devices) {
 }
 
 def refreshDevices() {
-	def body = apiGet("", null)
-	for (device in body?.vehicles) {
+	def body = apiGet("v2/user", "getdevice")
+	for (device in body?.devices) {
 		def dev = getChildDevice(buildDNI(device.deviceid))
+		if (dev != null) {
+			if (device.type != "Sleep Monitor") {
+				def intBattery = 30
+				if (device.battery == "high")
+					intBattery = 80
+				else if (device.battery == "medium")
+					intBattery = 50
+				else if (device.battery == "low")
+					intBattery = 20
+				dev.sendEvent(name: "battery", value: intBattery)
+			}
+		}
 	}
 }
 
